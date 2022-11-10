96b4b6fe786a29dbcf7810614f84f65c7dee0bf3
hdoc <blueguitarist1520@gmail.com>
2022-11-09 21:52:13 -0500

    Refactor: Transforn 'ncu' alias to a function for support msys terminal

--- plugins/npm_check_updates/npm_check_updates.sh
+++ plugins/npm_check_updates/npm_check_updates.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 
 #
+# FUNCTIONS
+#
+
+ncu () {
+  local to_execute=''
+
+  if [[ "$OSTYPE" == 'msys' ]]; then
+    to_execute='winpty ncu.cmd'
+  else
+    to_execute='ncu'
+  fi
+
+  eval "${to_execute} $*"
+}
+
+#
 # ALIASES
 #
 
-# TODO: User 'winpty ncu.cmd' on Windows and 'ncu' on Unix
-alias ncu='winpty ncu.cmd'
 alias ncuG='ncu -g'
