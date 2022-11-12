d40072875d1d145363fbb262011b076d440e03d9
hdoc <blueguitarist1520@gmail.com>
2022-11-11 22:41:52 -0500

    Refactor(vitejs): Run specific command for certain Node JS versions

--- plugins/vitejs/vitejs.sh
+++ plugins/vitejs/vitejs.sh
@@ -4,4 +4,10 @@
 # FUNCTIONS
 #
 
-vite () { npm create vite "$2" -- --template "$1"; }
+vite () {
+  if [[ "$(node --version)" =~ ^v([1-9]|10|11|12|13|14)\. ]]; then
+    npm create vite "$2" --template "$1"
+  else
+    npm create vite "$2" -- --template "$1"
+  fi
+}
