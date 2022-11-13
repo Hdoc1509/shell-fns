98486e758c5092fd023251593c2fb07eb9016ae5
hdoc <blueguitarist1520@gmail.com>
2022-11-12 19:04:46 -0500

    Refactor: Remove feedback when sourcing plugins

--- main.sh
+++ main.sh
@@ -12,9 +12,8 @@ if [[ -z "$SF_PLUGINS" ]]; then
   echo -e "${orange}Warning: No plugins selected${nocolor}"
 else
   for plugin in "${SF_PLUGINS[@]}"; do
-    echo -ne "${lightpurple}Sourcing plugin${nocolor} ${lightcyan}${plugin}${nocolor} ..."
     # shellcheck disable=SC1090
-    source "$SF_PATH"/plugins/"$plugin"/"$plugin".sh && echo -e "${lightgreen}done" || echo -e "{red}FAILED"
+    source "$SF_PATH"/plugins/"$plugin"/"$plugin".sh
   done
 fi
 
