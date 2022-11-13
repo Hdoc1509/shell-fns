9a28e2cf295e450c5d66452bbbf0e28ec08a2e00
hdoc <blueguitarist1520@gmail.com>
2022-11-12 19:09:52 -0500

    Feat(cmd_line): Add checking for SF_CAT variable

--- plugins/cmd_line/cmd_line.sh
+++ plugins/cmd_line/cmd_line.sh
@@ -20,7 +20,13 @@ SF_ls () {
 # ALIASES
 #
 
-alias cat='$SF_CAT'
+if [[ -z "$SF_CAT" ]]; then
+  # shellcheck disable=SC2154
+  echo -e "${orange}Warning: SF_CAT is unset${nocolor}"
+else
+  alias cat='$SF_CAT'
+fi
+
 alias cls='clear'
 alias ls='SF_ls'
 alias ll='ls -lh'
