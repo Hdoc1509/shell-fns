1bd05fae25a2d0be8cd2fddd1ed0600451f445ca
hdoc <blueguitarist1520@gmail.com>
2022-11-12 22:32:44 -0500

    Refactor(neovim): Simplify syntax for value assignment of 'to_execute' variable

--- plugins/neovim/neovim.sh
+++ plugins/neovim/neovim.sh
@@ -7,13 +7,8 @@
 nv_flag () { [[ "$1" == '.' ]] && echo "${SF_NV_FLAGS} ." || echo "-p $*"; }
 
 nv () {
-  local to_execute=''
-
-  if [[ "$OSTYPE" == 'msys' ]]; then
-    to_execute='winpty nvim'
-  else
-    to_execute='nvim'
-  fi
+  local to_execute=""
+  to_execute="$([[ "$OSTYPE" == 'msys' ]] && echo "winpty nvim" || echo "nvim")"
 
   eval "${to_execute} $(nv_flag "$@")"
 }
