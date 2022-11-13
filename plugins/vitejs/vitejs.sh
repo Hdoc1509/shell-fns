bf739611e085d3582a33d4a4a36b1d3b290805ab
hdoc <blueguitarist1520@gmail.com>
2022-11-12 19:43:08 -0500

    Feat(vitejs): Add specific checkings for Node JS version

--- plugins/vitejs/vitejs.sh
+++ plugins/vitejs/vitejs.sh
@@ -5,9 +5,14 @@
 #
 
 vite () {
-  if [[ "$(node --version)" =~ ^v([1-9]|10|11|12|13|14)\. ]]; then
+  local node_v=""; node_v="$(node --version)"
+
+  if [[ "$node_v" =~ ^v14\.(1[8-9]|2[0-1]) ]]; then
     npm create vite "$2" --template "$1"
-  else
+  elif [[ "$node_v" =~ ^v1[5-9] ]]; then
     npm create vite "$2" -- --template "$1"
+  else
+    # shellcheck disable=SC2154
+    echo -e "${red}ERROR: ${orange}Node ${node_v} doesn't support vitejs${nocolor}"
   fi
 }
