bb9ee9d48915bef42cac97003ea7c13c4819f021
hdoc <blueguitarist1520@gmail.com>
2022-11-12 23:17:56 -0500

    Fix(vitejs): Use specific Node JS binary for 'msys' terminal. Fixes #6

--- plugins/vitejs/vitejs.sh
+++ plugins/vitejs/vitejs.sh
@@ -5,7 +5,13 @@
 #
 
 vite () {
-  local node_v=""; node_v="$(node --version)"
+  local node_v="";
+
+  if [[ "$OSTYPE" == 'msys' ]]; then
+    node_v="$(node.exe --version)"
+  else
+    node_v="$(node --version)"
+  fi
 
   if [[ "$node_v" =~ ^v14\.(1[8-9]|2[0-1]) ]]; then
     npm create vite "$2" --template "$1"
