2be0f99076ef510aade07b3df26fb7d5ed299967
hdoc <blueguitarist1520@gmail.com>
2022-11-16 17:22:24 -0500

    Refactor(npm): Remove aliases 'npmSE', 'npmPkgs' and 'npmGPkgs'

--- plugins/npm/npm.sh
+++ plugins/npm/npm.sh
@@ -5,12 +5,9 @@
 #
 
 if [[ "$OSTYPE" == 'msys' ]]; then alias npm='winpty npm.cmd'; fi
-alias npmSE='npm i -E'
 alias npmDE='npm i -D -E'
+alias npmDev='npm run dev'
 alias npmG='npm i -g'
+alias npmT='npm test'
 alias npmU='npm uninstall'
 alias npmUG='npmU -g'
-alias npmT='npm test'
-alias npmDev='npm run dev'
-alias npmPkgs='npm list --depth 0'
-alias npmGPkgs='npmPkgs -g'
