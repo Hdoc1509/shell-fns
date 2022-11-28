bef40fac6e03f54027a14664226bb97f50a55242
hdoc <blueguitarist1520@gmail.com>
2022-11-28 11:00:50 -0500

    Refactor: Use 'if-else' statement for set alias 'sfCf'

--- main.sh
+++ main.sh
@@ -6,6 +6,8 @@ source "$SF_PATH"/colors.sh
 
 if [[ -z "$SF_EDITOR" ]]; then
   echo -e "${orange}Warning: SF_EDITOR is unset${nocolor}"
+else
+  alias sfCf='cd ${SF_PATH}; ${SF_EDITOR} .'
 fi
 
 if [[ -z "$SF_PLUGINS" || "${#SF_PLUGINS[@]}" == '0' ]]; then
@@ -15,10 +17,6 @@ else
     # shellcheck disable=SC1090
     source "$SF_PATH"/plugins/"$plugin"/"$plugin".sh
   done
-fi
-
-if [[ -n "$SF_EDITOR" ]]; then
-  alias sfCf='cd ${SF_PATH}; ${SF_EDITOR} .'
 fi
 
 if [[ "$SF_START_DEV_FOLDER" == 'true' ]]; then dev; fi
