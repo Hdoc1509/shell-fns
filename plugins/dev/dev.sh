0c8e6f769bf506cdeb4bfc80c65d614923ba4c88
hdoc <blueguitarist1520@gmail.com>
2022-12-04 13:10:28 -0500

    Refactor(dev): Exapand alias 'dev' when defined

--- plugins/dev/dev.sh
+++ plugins/dev/dev.sh
@@ -8,5 +8,6 @@ if [[ -z "$SF_DEV_FOLDER" ]]; then
   # shellcheck disable=SC2154
   echo -e "${orange}Warning: SF_DEV_FOLDER is unset${nocolor}"
 else
-  alias dev='cd ${SF_DEV_FOLDER}'
+  # shellcheck disable=SC2139
+  alias dev="cd ${SF_DEV_FOLDER}"
 fi
