60fc9d8a779bea5bb26d385b8da38a1cfa917263
hdoc <blueguitarist1520@gmail.com>
2022-12-04 18:30:31 -0500

    Refactor(nvm): Remove support for 'nvm-windows' and add more aliases

--- plugins/nvm/nvm.sh
+++ plugins/nvm/nvm.sh
@@ -4,21 +4,61 @@
 # ALIASES
 #
 
-alias nvmI='nvm install'
+alias nvmV='nvm --version'
 
-if [[ "$OSTYPE" == 'msys' ]]; then
-  alias nvmILts='nvm install lts'
-else
-  alias nvmILts='nvm install --lts'
-fi
+alias nvmi='nvm install'
+alias nvmin='nvm install node'
+alias nvmis='nvm install -s'
+alias nvmib='nvm install -b'
+alias nvmir='nvm install --reinstall-packages-from='
+alias nvmilts='nvm install --lts'
+alias nvmiltsn='nvm install --lts='
+alias nvmisf='nvm install --skip-default-packages'
+alias nvmiln='nvm install --latest-npm'
+alias nvminp='nvm install --no-progress'
+alias nvmid='nvm install --default'
 
-alias nvmLs='nvm ls'
-
-if [[ "$OSTYPE" == 'msys' ]]; then
-  alias nvmLsR='nvm ls available'
-else
-  alias nvmLsR='nvm ls-remote'
-fi
+alias nvmu='nvm uninstall'
+alias nvmults='nvm uninstall --lts'
+alias nvmultsn='nvm uninstall --lts='
 
 alias nvmU='nvm use'
-alias nvmUI='nvm uninstall'
+alias nvmUs='nvm use --silent'
+alias nvmUlts='nvm use --lts'
+alias nvmUltsn='nvm use --lts='
+
+alias nvme='nvm exec'
+alias nvmes='nvm exex --silent'
+alias nvmelts='nvm exec --lts'
+alias nvmeltsn='nvm exec --lts='
+
+alias nvmR='nvm run'
+alias nvmRs='nvm run --silent'
+alias nvmRlts='nvm run --lts'
+alias nvmRltsn='nvm run --lts='
+
+alias nvmc='nvm current'
+
+alias nvml='nvm ls'
+alias nvmlnc='nvm ls --no-colors'
+alias nvmlna='nvm ls --no-alias'
+
+alias nvmlr='nvm ls-remote'
+alias nvmlrlts='nvm ls-remote --lts'
+alias nvmlrltsn='nvm ls-remote --lts='
+alias nvmlrnc='nvm ls-remote --no-colors'
+
+alias nvmv='nvm version'
+alias nvmvr='nvm version-remote'
+alias nvmvrlts='nvm version-remote --lts'
+alias nvmvrltsn='nvm version-remote --lts='
+
+alias nvmd='nvm deactivate'
+alias nvmds='nvm deactivete --silen'
+
+alias nvma='nvm alias'
+alias nvmanc='nvm alias --no-colors'
+alias nvmua='nvm unalias'
+
+alias nvmw='nvm which'
+alias nvmws='nvm which --silent'
