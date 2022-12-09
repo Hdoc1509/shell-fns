c6e33851a94c62f7530126813578fecfa9025702
hdoc <blueguitarist1520@gmail.com>
2022-12-08 19:06:10 -0500

    Refactor(neovim_gui): Use function '__sf_nv_flag'

--- plugins/neovim_gui/neovim_gui.sh
+++ plugins/neovim_gui/neovim_gui.sh
@@ -6,6 +6,6 @@ source "$SF_PATH"/plugins/neovim/neovim.sh
 # FUNCTIONS
 #
 
-nvq () { eval "nvim-qt -- $(nv_flag "$@")"; }
+nvq () { eval "nvim-qt -- $(__sf_nv_flag "$@")"; }
 nvqNb () { nvq "$@" & /dev/null; }
-nvd () { eval "neovide --multigrid -- $(nv_flag "$@")"; }
+nvd () { eval "neovide --multigrid -- $(__sf_nv_flag "$@")"; }
