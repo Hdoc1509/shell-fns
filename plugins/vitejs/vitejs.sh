62a58b6087c3916f301a592f4359c9e06159eaf3
hdoc <blueguitarist1520@gmail.com>
2023-04-22 22:30:30 -0500

    refactor(vitejs): remove checking and warnings for incompatible nodejs versions

--- plugins/vitejs/vitejs.sh
+++ plugins/vitejs/vitejs.sh
@@ -3,22 +3,6 @@ if ! is_bin_in_path node; then
   return
 fi
 
-__node_v=''
-
-if [[ $OSTYPE == 'msys' ]]; then
-  __node_v=$(node.exe --version)
-else
-  __node_v=$(node --version)
-fi
-
-__node_v_mj="${__node_v:1:2}"
-__node_v_mn="${__node_v:4:2}"
-
-if ((__node_v_mj <= 13)) || { ((__node_v_mj == 14)) && ((__node_v_mn < 18)); }; then
-  SF_WARNS+=("[vitejs plugin]: Vitejs requires Node.js v14.18+ or v15+. Please upgrade your version of Node.js")
-  return
-fi
-
 #
 # FUNCTIONS
 #
