51afb962a6e91e2b31a22e5a2a7d5554148a29da
hdoc <blueguitarist1520@gmail.com>
2023-04-23 16:42:33 -0500

    refactor(vitejs): transform alias npvi to function

--- plugins/vitejs/vitejs.sh
+++ plugins/vitejs/vitejs.sh
@@ -7,6 +7,29 @@ fi
 # FUNCTIONS
 #
 
+npvi() {
+  local args_number=$#
+
+  local npm_v
+  local dir_name="${2}"
+  local template="${1}"
+
+  if ((args_number == 0)); then
+    npm create vite@latest
+  elif ((args_number == 2)); then
+    npm_v=$(npm --version)
+    npm_v_mj="${npm_v:0:1}"
+
+    if ((npm_v_mj == 6)); then
+      npm create vite@latest "${dir_name}" --template "${template}"
+    else
+      npm create vite@latest "${dir_name}" -- --template "${template}"
+    fi
+  else
+    echo -e "${LIGHTRED}[ SHELL-FNS ]: ERROR - Expected 0 or 2 arguments"
+  fi
+}
+
 vitejs() {
   local npm_v dir_name="${2}" template="${1}"
 
@@ -24,6 +47,5 @@ vitejs() {
 # ALIASES
 #
 
-alias npvi='npm create vite@latest'
 alias yavi='yarn create vite'
 alias pnvi='pnpm create vite'
